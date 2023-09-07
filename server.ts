3c66bf334480b81f157ce8e298ca6e5a90453719
bisari <bisari31@gmail.com>
2023-09-07 13:28:17 +0900

    chore: import 구문 적용

--- server.ts
+++ server.ts
@@ -1,31 +1,31 @@
-const http = require("http");
-const https = require("https");
-const fs = require("fs");
-const { parse } = require("url");
-const next = require("next");
+import http from 'http';
+import https from 'https';
+import fs from 'fs';
+import next from 'next';
+import { parse } from 'url';
 
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== 'production';
 const PORT = 3001;
-const app = next({ dev, hostname: "localhost", port: PORT });
+const app = next({ dev, hostname: 'localhost', port: PORT });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   http
     .createServer((req, res) => {
-      const parsedUrl = parse(req.url, true);
+      const parsedUrl = parse(req.url ?? '', true);
       handle(req, res, parsedUrl);
     })
     .listen(PORT, () => {
       console.log(`> Ready on http://localhost:${PORT}`);
     });
   const options = {
-    key: fs.readFileSync("./localhost-key.pem"),
-    cert: fs.readFileSync("./localhost.pem"),
+    key: fs.readFileSync('./localhost-key.pem'),
+    cert: fs.readFileSync('./localhost.pem'),
   };
 
   https
     .createServer(options, (req, res) => {
-      const parsedUrl = parse(req.url, true);
+      const parsedUrl = parse(req.url ?? '', true);
       handle(req, res, parsedUrl);
     })
     .listen(PORT + 1, () => {
