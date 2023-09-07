3aef4fb6a6188022e5e82e32a12477798892bf04
bisari <bisari31@gmail.com>
2023-09-08 03:21:08 +0900

    rename: ReduxProviders 파일명 변경

--- src/redux/ReduxProviders.tsx
+++ src/redux/ReduxProviders.tsx
@@ -3,6 +3,6 @@
 import { Provider } from 'react-redux';
 import { store } from './store';
 
-export const Providers = ({ children }: React.PropsWithChildren) => {
+export const ReduxProviders = ({ children }: React.PropsWithChildren) => {
   return <Provider store={store}>{children}</Provider>;
 };
