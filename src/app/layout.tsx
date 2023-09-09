86b9967cd0349ba42437cc41163859928c774441
bisari <bisari31@gmail.com>
2023-09-10 06:28:31 +0900

    chore: meta verification 등록

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,12 @@ const notoSans = Noto_Sans_KR({
 export const metadata: Metadata = {
   title: '로또 당첨 조회',
   description: '가짜 로또 1등 당첨 앱',
+  verification: {
+    google: 'e-TNPhd8w4G89P7qHvpbNUMOzmwlO8jX-6zAin2jbK8',
+    other: {
+      'naver-site-verification': 'fb43b2b0ff24ab6bdcbb4b129045a18a86f4ce06',
+    },
+  },
 };
 
 export default function RootLayout({
