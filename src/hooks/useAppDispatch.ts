04f3248b6e1f78bd6b7b9d5964a99fdff80aaf9f
bisari <bisari31@gmail.com>
2023-09-10 05:08:43 +0900

    style: lotto slice 초기 상태 변경

--- src/hooks/useAppDispatch.ts
+++ src/hooks/useAppDispatch.ts
@@ -2,6 +2,7 @@ import { AppDispatch } from '@/redux/store';
 import { useDispatch } from 'react-redux';
 
 type DispatchFunc = () => AppDispatch;
+
 const useAppDispatch: DispatchFunc = useDispatch;
 
 export default useAppDispatch;
--- src/redux/slices/lottoSlice.ts
+++ src/redux/slices/lottoSlice.ts
@@ -2,25 +2,22 @@ import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 
 type InitialState = {
   selectedRank: Rank;
-  totalPrize: number;
 };
 
 const initialState: InitialState = {
   selectedRank: 1,
-  totalPrize: 0,
 };
 
 const lottoSlice = createSlice({
   name: 'lotto',
   initialState,
   reducers: {
-    setRank(state, action: PayloadAction<Rank>) {
+    updateRank(state, action: PayloadAction<Rank>) {
       state.selectedRank = action.payload;
-    },
-    sumPrize(state, action: PayloadAction<number>) {
-      state.totalPrize += action.payload;
     },
   },
 });
 
 export default lottoSlice.reducer;
+
+export const { updateRank } = lottoSlice.actions;
